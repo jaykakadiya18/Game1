9c6d6ed9702524a5e3dd4a86cfd21af31ab6e745
Jay kakadiya <58882863+jaykakadiya18@users.noreply.github.com>
2020-07-20 16:16:47 +0530

    Update and rename def.py to GNG.py
    V 1.1

--- GNG.py
+++ GNG.py
@@ -1,8 +1,8 @@
 n=18
-print("gusse game")
+print("guessing number game")
 
 for i in range(4):
-    x=int(input("Gusse Your number in 1 to 50:"))
+    x=int(input("Guess Your number in 1 to 50:"))
     if x<=50 and x>0:
         if x<n:
             print("your number is very low")
